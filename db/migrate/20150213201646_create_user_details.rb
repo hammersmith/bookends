50b2e85d54e81d2d2561c394a1694b89a1cb3928
John Rodkey <rodkey@westmont.edu>
2015-02-13 12:24:15 -0800

    jr/cr - add User_detail model

---
+++ db/migrate/20150213201646_create_user_details.rb
@@ -0,0 +1,20 @@
+class CreateUserDetails < ActiveRecord::Migration
+  def change
+    create_table :user_details do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :lt_name
+      t.string :work_country
+      t.string :how_referred
+      t.string :kid_info
+      t.string :book_prefs
+      t.Boolean :homeschooled
+      t.string :delivery_method
+      t.integer :primary_ship_addr
+      t.integer :secondary_ship_addr
+      t.string :courier_email
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/factories/user_details.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :user_detail do
+    first_name "MyString"
+last_name "MyString"
+lt_name "MyString"
+work_country "MyString"
+how_referred "MyString"
+kid_info "MyString"
+book_prefs "MyString"
+homeschooled ""
+delivery_method "MyString"
+primary_ship_addr 1
+secondary_ship_addr 1
+courier_email "MyString"
+  end
+
+end
