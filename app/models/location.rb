67eb804d5ee8950a4b4a70aaafe7e577d7f27a43
Aaron Panchal <aaron.panchal@appfolio.com>
2015-02-14 09:28:51 -0800

    ap - add validations to locations and tests

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,5 @@
 class Location < ActiveRecord::Base
   has_many :works
+
+  validates :name, :shelf, :color, presence: true
 end
--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class LocationTest < ModelTestCase
+
+  test 'validate presence of attributes' do
+    location = Location.new
+    assert_not_predicate location, :valid?
+    assert_equal ["can't be blank"], location.errors.messages[:name]
+    assert_equal ["can't be blank"], location.errors.messages[:shelf]
+    assert_equal ["can't be blank"], location.errors.messages[:color]
+  end
+
 end
