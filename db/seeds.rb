74e7efda324088920f4cb8604e52174d4a955b4b
John Rodkey <rodkey@westmont.edu>
2015-02-13 20:42:32 -0800

    jr - add seed mechanism for locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+CSV.foreach(Rails.root.join('db/seeds_data/location_seed.csv')) do |row|
+  begin
+    Location.find_or_create_by(name: row[0], color: row[1], shelf: row[2])
+  rescue
+    puts "Error #{$!}"
+  end
+end
